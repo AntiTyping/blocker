29bd3a276f7dd645353e661dcf91adc5369d8d59
Andy Pliszka <andy@isp44.com>
2025-02-19 17:56:20 -0500

    feat: adds crypto keys

---
+++ crypto/crypto_test.go
@@ -0,0 +1,51 @@
+package crypto
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+
+	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+
+	pubKey := privKey.Public()
+
+	assert.Equal(t, len(pubKey.Butes()), pubKeyLen)
+}
+
+func TestNewPrivateKeyFromString(t *testing.T) {
+	seed := "f270acaebd8a7153a38d61e8f64eb0f2695745d72501725ca4a9632818fbbd1e"
+	privKey := NewPrivateKeyFromString(seed)
+
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+
+	assert.Equal(t, "b0a03971455014435c4c833e20053b4e281c65bb", privKey.Public().Address().String())
+}
+
+func TestPrivateKeySing(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	msg := []byte("foo bar")
+
+	sig := privKey.Sign(msg)
+
+	assert.True(t, sig.Verify(privKey.Public(), msg))
+
+	// test with invalid msg
+	assert.False(t, sig.Verify(privKey.Public(), []byte("foo")))
+
+	// test with invalid key
+	assert.False(t, sig.Verify(GeneratePrivateKey().Public(), msg))
+}
+
+func TestPublicKeyToAddress(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	address := pubKey.Address()
+
+	assert.Equal(t, addressLen, len(address.Bytes()))
+	fmt.Println(address)
+}
