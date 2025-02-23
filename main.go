4a457056e9c3feb97f1d57c935a2006662eafee8
Andy Pliszka <andy@isp44.com>
2025-02-23 12:14:23 -0500

    feat: adds handshake between nodes

--- main.go
+++ main.go
@@ -1,12 +1,66 @@
 package main
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+import (
+	"blocker/node"
+	"blocker/proto"
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
+	node := node.NewNode()
+	fmt.Println("Start")
+	//opts := []grpc.Option{}
+	//grpcServer, err := gr()
+	opts := []grpc.ServerOption{}
+	grpcSerer := grpc.NewServer(opts...)
+	ln, err := net.Listen("tcp", ":4000")
+	if err != nil {
+		panic(err)
+	}
+	proto.RegisterNodeServer(grpcSerer, node)
+	fmt.Println("node running on port: ", ":4000")
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			makeTransaction()
+
+		}
+	}()
+	go func() {
+		for {
+			time.Sleep(time.Second * 5)
+			makeTransaction()
+
+		}
+	}()
+	err = grpcSerer.Serve(ln)
+	if err != nil {
+		panic(err)
+	}
+
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func makeTransaction() {
+	client, err := grpc.Dial(":4000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	c := proto.NewNodeClient(client)
+
+	tx := &proto.Version{
+		Version: "blocker-1",
+		Height:  0,
+	}
+
+	_, err = c.Handshake(context.Background(), tx)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ node/node.go
@@ -0,0 +1,38 @@
+package node
+
+import (
+	"blocker/proto"
+	"context"
+	"fmt"
+
+	peer "google.golang.org/grpc/peer"
+)
+
+type Node struct {
+	version string
+	proto.UnimplementedNodeServer
+}
+
+func NewNode() *Node {
+	return &Node{
+		version: "blocker-1",
+	}
+}
+
+func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*proto.Ack, error) {
+	peer, _ := peer.FromContext(ctx)
+	fmt.Println("Received transaction from: ", peer)
+	return &proto.Ack{}, nil
+}
+
+func (n *Node) Handshake(ctx context.Context, v *proto.Version) (*proto.Version, error) {
+	ourVersion := &proto.Version{
+		Version: n.version,
+		Height:  100,
+	}
+
+	peer, _ := peer.FromContext(ctx)
+
+	fmt.Printf("received version from %s: %+v\n", peer, v)
+	return ourVersion, nil
+}
